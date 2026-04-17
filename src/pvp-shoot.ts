c52399cad38f443526236e1b7dead2d1dfae906c
Paweł Więckowski <pawel.wieckowski@schange.com>
2026-04-17 13:04:22 +0200

    Add cooldown to pvp shoot

--- src/pvp-shoot.ts
+++ src/pvp-shoot.ts
@@ -1,9 +1,10 @@
-import { TEAM } from "./constants";
-import { Player } from "./types/haxball-api";
+import { PlayerId } from "./types/haxball-api";
 import { discsDistance } from "./utils/geometry";
 import { Plugin } from "./utils/plugin";
 
-export const pvpShootPlugin: Plugin = room => {
+export const pvpShootPlugin: Plugin = (room) => {
+  const playerCooldowns: Record<PlayerId, number> = {};
+  const cooldownTime = 5000;
   return {
     onGameStart: () => {
       const code = `
@@ -23,30 +24,54 @@ export const pvpShootPlugin: Plugin = room => {
             shoot()
           }
         })
-      `
-      room.sendAnnouncement(`code:`)
-      room.sendAnnouncement(code, undefined, 0xAAAAAA, "italic", 1)
+      `;
+      room.sendAnnouncement(`code:`);
+      room.sendAnnouncement(code, undefined, 0xaaaaaa, "italic", 1);
     },
     onPlayerChat: (player, message) => {
       if (message.startsWith("!shoot")) {
-        const opponents = room.getPlayerList().filter(p => p.team !== player.team)
+        if (
+          playerCooldowns[player.id] &&
+          playerCooldowns[player.id] > Date.now()
+        ) {
+          room.sendAnnouncement(
+            `You must wait ${Math.floor((playerCooldowns[player.id] - Date.now()) / 1000)} seconds before shooting again`,
+            player.id,
+          );
+          return;
+        }
+        playerCooldowns[player.id] = Date.now() + cooldownTime;
+        const opponents = room
+          .getPlayerList()
+          .filter((p) => p.team !== player.team);
         if (opponents.length > 0) {
-          const [,closestOpponent] = opponents.slice(1).reduce(([closestDistance, closestOpponent], opponent) => {
-            const distance = discsDistance(player.position, opponent.position)
-            return distance < closestDistance ? [distance, opponent] : [closestDistance, closestOpponent]
-          }, [Infinity, opponents[0]] satisfies [number, Player])
+          const [, closestOpponent] = opponents.slice(1).reduce(
+            ([closestDistance, closestOpponent], opponent) => {
+              const distance = discsDistance(
+                player.position,
+                opponent.position,
+              );
+              return distance < closestDistance
+                ? [distance, opponent]
+                : [closestDistance, closestOpponent];
+            },
+            [Infinity, opponents[0]],
+          );
 
-          const angle = Math.atan2(closestOpponent.position.y - player.position.y, closestOpponent.position.x - player.position.x)
-          const bulletId = message.split(" ")[1] || 1
+          const angle = Math.atan2(
+            closestOpponent.position.y - player.position.y,
+            closestOpponent.position.x - player.position.x,
+          );
+          const bulletId = message.split(" ")[1] || 1;
           room.setDiscProperties(+bulletId, {
             x: player.position.x,
             y: player.position.y,
             xspeed: Math.cos(angle) * 10,
             yspeed: Math.sin(angle) * 10,
-            color: player.team === TEAM.RED ? 0xFF0000 : 0x0000FF,
+            color: player.team === 1 ? 0xff0000 : 0x0000ff,
           });
         }
       }
-    }
-  }
+    },
+  };
 };
